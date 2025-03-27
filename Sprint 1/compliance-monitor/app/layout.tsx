72f648b90bbf823c8b51eb14108fed50402c8eda
Chris Toph <36116606+TophC7@users.noreply.github.com>
2025-03-27 01:12:10 -0400

    Refactor layout and navbar components; enhance login functionality and update site configuration

--- Sprint 1/compliance-monitor/app/layout.tsx
+++ Sprint 1/compliance-monitor/app/layout.tsx
@@ -1,14 +1,13 @@
 import '@/styles/globals.css'
-import { Link } from '@heroui/link'
-import clsx from 'clsx'
-import { Metadata, Viewport } from 'next'
-
-import { Providers } from './providers'
-
 import { fontSans } from '@/config/fonts'
 import { siteConfig } from '@/config/site'
 import { FhirClientProvider } from '@Components/FHIR/FHIRClientProvider'
+import { Navbar } from '@Components/navbar'
+import { Link } from '@heroui/link'
+import clsx from 'clsx'
+import { Metadata, Viewport } from 'next'
 import React from 'react'
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
 	title: {
@@ -28,17 +27,14 @@ export const viewport: Viewport = {
 	]
 }
 
-export default function RootLayout({
-	children
-}: {
-	children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html suppressHydrationWarning lang="en">
 			<head />
 			<body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
 				<Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
 					<FhirClientProvider>
+						<Navbar />
 						<div className="relative flex flex-col h-screen">
 							<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
 							<footer className="w-full flex items-center justify-center py-3">
