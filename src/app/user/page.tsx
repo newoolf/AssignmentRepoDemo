e8177eae8d35da5bbb06a63b9e443a46c0f70d8d
kaden <brownk46@vcu.edu>
2025-04-09 21:25:33 -0400

    medication profile UI update. PreDev 4

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,6 +4,7 @@ import { title } from "@/components/primitives";
 import { useMedication } from "@Lib/api/MedicationService";
 import { usePatient } from "@Lib/api/PatientService";
 import { subtitle } from "@/components/primitives";
+import Image from'next/image'
 
 
 export default function UserProfilePage() {
@@ -68,20 +69,49 @@ export default function UserProfilePage() {
 		medicationImport();
 		console.log("Medication List after import: ", medicationList);
 		setMedications(medicationList);
-	  
+  
 	}
   }, [medication, error, loading]);
 
-  return (
-    <div>
-      <p className={title()}>
-        Login<span className="text-green-500"> SUCCESSFUL!</span>
-        </p>
-      {/* Display patient data */}
-      <p className={`${subtitle()} text-white-600 text-2xl mt-4`}>Patient's Medications:</p>
+  const fullName=''
+  const dob=''
+  const patient_id =''
 
-      <div className="bg-white p-4 rounded-xl shadow-md mt-2 h-999">
-        <ul className="list-disc pl-6 text-gray-700">
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start py-10 px-4">
+      <div className="flex items-center space-x-4 mb-6">
+      <p className={title()}>
+        <span className="text-black">Login</span>{' '}
+        <span className="text-green-500"> SUCCESSFUL!</span>
+
+        </p>
+        <Image
+          src="/orangepillbottle.jpg"
+          alt="Pill Bottle"
+          width={100}
+          height={100}
+          className="rounded-md shadow-md"
+          />
+        </div>      
+
+      
+       
+      {/* Display patient data */}
+    <div className="bg-white rounded-2xl shadow-lg p-6 w-full max-w-xl mb-6">
+       <p className="text-black ${subtitle()}text-2xl font-bold mt-4"> Patient's Information:</p>
+        <div className="text-gray-700 mb-1 space-y-1">
+          <p><span className="font-medium">Name:</span> {fullName}</p>
+          <p><span className="font-medium">Date of Birth:</span> {dob}</p>
+          <p><span className="font-medium">Patient ID:</span> {patient_id}</p>
+        </div>
+      </div>
+
+      {/* <div className="bg-white p-4 rounded-xl shadow-md mt-2 h-999"> */}
+      <div className="bg-white rounded-2xl shadow-lg p-6 w-full max-w-xl">
+        <p className="text-xl font-bold text-black mb-4">Patient's Medications:</p>
+   
+        
+        <ul className="list-disc pl-4 space-y-2 text-gray-700">
           
           {medications.map((med,index) => (
             <li key={index}>{med}</li>
