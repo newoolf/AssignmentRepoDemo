6328b81d7f459ad30689a9db434abb4ca0aa4c18
dzuluin <luinsart@gmail.com>
2025-04-26 20:16:01 -0400

    Dashboard API integration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,22 +4,65 @@ import { Card, CardBody } from '@heroui/card'
 import { Avatar } from '@heroui/avatar'
 import { UserIcon } from '@/components/icons'
 import { Divider } from '@heroui/react'
+import { usePatient } from '@Lib/api/PatientService'
+import { useEffect, useState } from 'react'
 import MedicationManagementCard from '@Components/medication-management-card'
 
 export default function DashboardPage() {
+  const [fhirClient, setFhirClient] = useState<any>(null)
+  const { patient, error: patientError, loading: patientLoading } = usePatient(fhirClient)
+
   // TODO: get actual user data from FHIR
-  const user = {
-    name: 'John Doe'
-  }
+  const [patientName, setFullName] = useState<string>('')
+  const [patientAddress, setAddress] = useState<string>('')
+  const [patientPhone, setPhone] = useState<string>('')
+  const [dob, setDOB] = useState<string>('')
+  //const [gp, setGP] = useState<string>('')
+  const [gpOrg, setGPOrg] = useState<string>('')
 
   // TODO: fix with relevant info from FHIR
   const details = [
-    { label: 'Email', value: 'john.doe@example.com' },
-    { label: 'Role', value: 'Administrator' },
-    { label: 'Member Since', value: 'January 2020' },
-    { label: 'Location', value: 'New York' },
-    { label: 'Last Login', value: 'March 5, 2024' }
+    { label: 'Phone Number', value: patientPhone },
+    { label: 'Email', value: 'Unknown' },
+    { label: 'Date of Birth', value: dob },
+    { label: 'Location', value: patientAddress }
   ]
+
+  useEffect(() => {
+    // Dynamically load the FHIR client library and set up client
+    const loadFHIRClient = async () => {
+      const FHIR = await import('fhirclient')
+      const client = await FHIR.oauth2.ready()
+      setFhirClient(client)
+    }
+    loadFHIRClient()
+  }, []) // This only runs once when the component mounts
+
+  useEffect(() => {
+    if (patient) {
+      const patientImport = async () => {
+        try {
+          const patientName = `${patient.name?.[0]?.given?.join(' ')} ${patient.name?.[0].family}` || 'Unknown Name'
+          const patientAddress = `${patient.address?.[0]?.city}` || 'Unknown Location'
+          const patientPhone = `${patient.telecom?.[0]?.value}` || 'Unknown Phone Number'
+          const dob = `${patient.birthDate}` || 'Unknown Birth Date'
+
+          setFullName(patientName)
+          setPhone(patientPhone)
+          setAddress(patientAddress)
+          setDOB(dob)
+        } catch (err) {
+          console.error('Error fetching patient data:', err)
+          setFullName('Error loading patient name')
+          setPhone('Error loading patient phone number')
+          setAddress('Error loading patient address')
+          setDOB('Error loading patient date of birth')
+        }
+      }
+
+      patientImport()
+    }
+  }, [patient])
 
   return (
     <main className="space-y-12">
@@ -32,7 +75,7 @@ export default function DashboardPage() {
               </Avatar>
             </div>
             <div className="flex flex-col w-full">
-              <h1 className="text-3xl md:text-4xl font-bold">{user.name}</h1>
+              <h1 className="text-3xl md:text-4xl font-bold">{patientName}</h1>
               <p className="text-base md:text-lg">Welcome to your dashboard</p>
               <Divider className="m-1" />
 
